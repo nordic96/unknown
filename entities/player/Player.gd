0dd8364e59e78f28294e33b8f336fee2a9404b5c
nordic96 <rhrlgns96@gmail.com>
2024-05-14 22:19:37 +0900

    restructured project

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -17,7 +17,7 @@ func _process(delta):
 	input.x = Input.get_axis('move_left', 'move_right')
 	input.z = Input.get_axis('move_forward', 'move_back')
 	
-	apply_central_force(input * 1200.0 * delta)
+	apply_central_force(twist_pivot.basis * input * 1200.0 * delta)
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
