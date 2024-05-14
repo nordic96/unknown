0f75dab83c8c7a6686fa591509b50661fe50c90d
nordic96 <rhrlgns96@gmail.com>
2024-05-14 14:51:11 +0900

    added pivot camera nodes for test player rigidbody

--- Player.gd
+++ Player.gd
@@ -1,13 +1,28 @@
 extends RigidBody3D
 
+var mouse_sensitivity := 0.001
+var twist_input := 0.0
+var pitch_input := 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# 1.0 if arrow up is pressed
-	var input = Input.get_action_strength("ui_up");
-	apply_central_force(input * Vector3.FORWARD * 1200.0 * delta);
+	var input := Vector3.ZERO
+	input.x = Input.get_axis('move_left', 'move_right')
+	input.z = Input.get_axis('move_forward', 'move_back')
+	
+	apply_central_force(input * 1200.0 * delta)
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	$TwistPivot.rotate_y(twist_input)
+	
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		twist_input = - event.relative.x * mouse_sensitivity
+		pitch_input = - event.relative.y * mouse_sensitivity
+		
