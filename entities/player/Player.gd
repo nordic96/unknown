76860fc5c6b2779a78d129c572bea5bbd7467976
nordic96 <rhrlgns96@gmail.com>
2024-05-15 09:01:55 +0900

    restructured project

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -1,9 +1,12 @@
 extends RigidBody3D
 
+const JUMP_FORCE = 4
+
 var mouse_sensitivity := 0.001
 var twist_input := 0.0
 var pitch_input := 0.0
 
+# Camera Nodes
 @onready var twist_pivot = $TwistPivot
 @onready var pitch_pivot = $TwistPivot/PitchPivot
 
@@ -11,21 +14,29 @@ var pitch_input := 0.0
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var input := Vector3.ZERO
+# Player Movements (walking)
+func handle_movement(delta):
+	var input := Vector3.ZERO 
 	input.x = Input.get_axis('move_left', 'move_right')
 	input.z = Input.get_axis('move_forward', 'move_back')
-	
 	apply_central_force(twist_pivot.basis * input * 1200.0 * delta)
-	if Input.is_action_just_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
+	if Input.is_action_pressed("jump"):
+		self.apply_impulse(Vector3.UP * JUMP_FORCE)
+		
+
+func handle_camera_movement():
 	twist_pivot.rotate_y(twist_input)
 	pitch_pivot.rotate_x(pitch_input)
 	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, -0.5, 0.5)
 	twist_input = 0
 	pitch_input = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	handle_camera_movement()
+	handle_movement(delta)
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
 
 func _unhandled_input(event):
